8ff3db4cf4d4be1d507301a12fc36d3b6ba0dfd4
Mapala1965 <mapalamegha@gmail.com>
2021-05-01 14:26:14 -0400

    second commit

--- src/welcome/Hello_World.java
+++ src/welcome/Hello_World.java
@@ -5,6 +5,8 @@ public class Hello_World {
 	public static void main(String[] args) {
 		
 		System.out.println("welcome to HelloWorld;;;;;;;!!!!!!$$%%%%");
+		
+		System.out.println(".......welcome to new Invention!!!!!!!>>>>>>>");
 
 	}
 
